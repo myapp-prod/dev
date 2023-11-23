bdf060c9cb7e3aee3b8eadfb2142fd0d9e8da2fe
rahul <azuredevopst@gmail.com>
2023-11-23 12:10:02 +0530

    code updated

--- function_app.py
+++ function_app.py
@@ -20,6 +20,6 @@ def mytrigger(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "Hi Rahul WELCOME TO AZURE FUCTIONS .",
              status_code=200
         )
